90cbf4a7f8a92cb2e14e2d954f9522d8823f964f
unknown <gdynamic01@gmail.com>
2019-11-30 21:04:37 +0100

    maj fic configTest

--- src/test/java/com/test/config/ConfigTestImo.java
+++ src/test/java/com/test/config/ConfigTestImo.java
@@ -5,7 +5,6 @@ package com.test.config;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +14,7 @@ import imo.com.ImoBackApplication;
  * @author balde
  *
  */
-@PropertySource(value = "classpath:application.properties")
+// @PropertySource(value = "classpath:application.properties")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = { ImoBackApplication.class })
 @Transactional
